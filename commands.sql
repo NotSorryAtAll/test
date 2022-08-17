2c1845160a221ab915fe440cc3ba04d06bae4465
NotSorryAtAll <109743646+NotSorryAtAll@users.noreply.github.com>
2022-08-17 14:10:57 +0100

    Update commands.sql

--- commands.sql
+++ commands.sql
@@ -1,52 +1,45 @@
 CREATE TABLE IF NOT EXISTS MedicalRecords(
     MedicalRecordsID INTEGER PRIMARY KEY AUTOINCREMENT,
-    Patient_ID INTEGER references patients,
+    Patients_id INT,
     Allergies VarChar(25),
     Previous_Condition VarChar(30),
     Previous_Medication VarChar(30),
     Vaccines VarChar (30),
-    Previous_Appointments VarChar(50)
+    Previous_Appointments VarChar(50),
+    FOREIGN KEY (Patients_id) REFERENCES patients (id)
+
+);
+
+CREATE TABLE IF NOT EXISTS patients (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    First_Name VarChar(100) ,
+    Last_Name VarChar(100),
+    DOB INT ,
+    Address TEXT ,
+    Contact_number INT,
+    Appointment_id INT
 );
 
 CREATE TABLE IF NOT EXISTS GP (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    appointment_id INT NOT NULL,
-    Allergies VARCHAR(25),
-    PreviousCondition VARCHAR(30),
-    PreviousMedication VARCHAR(30),
-    GP_name VARCHAR(100) NOT NULL,
-    GP_address VARCHAR(120) NOT NULL,
-    FOREIGN KEY (appointment_id) REFERENCES Appointment (appointment_id)
-    FOREIGN KEY (Allergies) REFERENCES MedicalRecords (Allergies)
-    FOREIGN KEY (PreviousCondition) REFERENCES MedicalRecords (PreviousCondition)
-    FOREIGN KEY (PreviousMedication) REFERENCES MedicalRecords (PreviousMedication)
+    GP_name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE  IF NOT EXISTS Appointment (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    gp_id INTEGER REFERENCES gp NOT NULL,
-    patient_id INTEGER REFERENCES patients NOT NULL,
+    gp_id INT,
+    patient_id INT,
     appointment_date_time DATETIME,
     room INTEGER NOT NULL,
     start_time TIME,
-    end_time TIME
+    end_time TIME,
+    FOREIGN KEY (gp_id) REFERENCES GP (id)
+    FOREIGN KEY (patient_id) REFERENCES patients (id)
+
 );
-
-CREATE TABLE IF NOT EXISTS patients (
-    ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    Patient_id NOT NULL,
-    First_Name TEXT NOT NULL,
-    Last_Name TEXT NOT NULL,
-    DOB NUM NOT NULL,
-    Address TEXT NOT NULL,
-    Contact_number  NOT NULL,
-    Appointment_id NOT NULL
-);
+INSERT into patients (id, first_name, last_name, DOB, address, contact_number, appointment_id) VALUES (NULL, 'NULTimothy', 'Davidson', '15-08-1998', '25 Hill Top Road, Coventry, CV10 6AO', '07457851499', '1');
+INSERT INTO medicalrecords VALUES (NULL, 1, "Grass", "None", "None", "1", "None");
+INSERT INTO GP VALUES (NULL, "BOB");
+INSERT INTO Appointment VALUES (NULL, 1, 1, "30/08/2022", "room B", "13:00", "13:30");
 
 
---INSERT INTO Appointment VALUES (NULL, NULL, NULL, '2022-08-13', '03', '8:30:00', '8:50:00');
-
- 
- 
---INSERT into patients (id, patient_id, first_name, last_name, DOB, address, contact_number, appointment_id) VALUES ('6', '6', 'Aimee', 'Hamilton', '18-05-1961', '29 Essex Road, Tangwick, ZE2 7XS', '07902845965', '6');
- 
